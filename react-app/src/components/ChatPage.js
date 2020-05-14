ce9f0c5d95e89c892c6d2c436d0fc7b3bf7b5cf2
Chad Bohannan <chad.bohannan@gmail.com>
2020-05-14 16:06:55 -0700

    react-app is stubbed.

---
+++ react-app/src/components/ChatPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ChatPage extends React.Component {
+    render() {
+        return (
+            <div style={{fontSize:40}}>
+                TODO
+            </div>
+        );
+    }
+}
+
+export default ChatPage;
